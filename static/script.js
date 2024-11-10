d01b1c0429c04a727783b3d776eb172dd9722e69
Kevin Japa <84851902+kevinjapa@users.noreply.github.com>
2024-11-09 23:30:39 -0500

    Mejorar la visualización de resultados y automatizar la presentación de imágenes en el formulario de carga

--- static/script.js
+++ static/script.js
@@ -23,15 +23,37 @@ document.getElementById("uploadForm").onsubmit = async function(event) {
     const kernelOutput = document.getElementById("Mascara");
     kernelOutput.textContent = "Mascara Utilizada: "+kernelInput.value;
 
+    //  para automatizar los resultados e imagen
+
+
+    // Mostrar las imágenes
+    document.querySelector(".imagen").style.display = "block";
+    // document.getElementById("original").src = "/" + result.original;
+    // document.getElementById("processed").src = "/" + result.processed;
+    // document.getElementById("original").style.display = "block";
+    // document.getElementById("processed").style.display = "block";
+
+    // Mostrar los resultados
+    document.querySelector(".titulos").style.display = "block";
+    document.querySelector(".resultado").style.display = "block";
+    // document.getElementById("kernel-size-display").textContent = result.kernel_size;
+    // document.getElementById("bloques").textContent = "Número de Bloques: " + result.bloques;
+    // document.getElementById("hilos").textContent = "Número de Hilos: " + result.hilos;
+    // document.getElementById("Procesamiento").textContent = "Tiempo de Procesamiento: " + result.procesamiento;
+
+    // Mostrar el contenedor de imágenes
+    // document.getElementById("images").style.display = "block";
+
 };
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     const kernelInput = document.getElementById("kernel_size");
-//     const kernelOutput = document.getElementById("Mascara");
+document.addEventListener("DOMContentLoaded", function() {
+    // const kernelInput = document.getElementById("kernel_size");
+    // const kernelOutput = document.getElementById("Mascara");
 
-//     kernelInput.addEventListener("input",function(){
-//         kernelOutput.textContent = "Mascara Utilizada: "+kernelInput.value;
+    // kernelInput.addEventListener("input",function(){
+    //     kernelOutput.textContent = "Mascara Utilizada: "+kernelInput.value;
 
-//     });
-// });
+    // });
+    document.getElementById("original").textContent = " ";
+});
 
