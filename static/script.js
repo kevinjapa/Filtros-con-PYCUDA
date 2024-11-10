5bb48745857b1ff0fa5e29a22772546456901d20
Kevin Japa <84851902+kevinjapa@users.noreply.github.com>
2024-11-10 12:38:05 -0500

    Mejorar la automatización de resultados y la visualización de imágenes en el formulario de carga

--- static/script.js
+++ static/script.js
@@ -21,28 +21,16 @@ document.getElementById("uploadForm").onsubmit = async function(event) {
 
     const kernelInput = document.getElementById("kernel_size");
     const kernelOutput = document.getElementById("Mascara");
-    kernelOutput.textContent = "Mascara Utilizada: "+kernelInput.value;
-
-    //  para automatizar los resultados e imagen
-
-
-    // Mostrar las imágenes
-    document.querySelector(".imagen").style.display = "block";
-    // document.getElementById("original").src = "/" + result.original;
-    // document.getElementById("processed").src = "/" + result.processed;
-    // document.getElementById("original").style.display = "block";
-    // document.getElementById("processed").style.display = "block";
+    kernelOutput.textContent = "Mascara Utilizada: "+kernelInput.value + " X " + kernelInput.value;
 
     // Mostrar los resultados
     document.querySelector(".titulos").style.display = "block";
     document.querySelector(".resultado").style.display = "block";
-    // document.getElementById("kernel-size-display").textContent = result.kernel_size;
-    // document.getElementById("bloques").textContent = "Número de Bloques: " + result.bloques;
-    // document.getElementById("hilos").textContent = "Número de Hilos: " + result.hilos;
-    // document.getElementById("Procesamiento").textContent = "Tiempo de Procesamiento: " + result.procesamiento;
-
-    // Mostrar el contenedor de imágenes
-    // document.getElementById("images").style.display = "block";
+    document.getElementById("kernel-size-display").textContent = result.kernel_size;
+    document.getElementById("bloques").textContent = "Número de Bloques: " + result.bloques;
+    document.getElementById("hilos").textContent = "Número de Hilos: " + result.hilos;
+    document.getElementById("Procesamiento").textContent = "Tiempo de Procesamiento: " + result.procesamiento;
+    document.getElementById("images").style.display = "block";
 
 };
 
@@ -54,6 +42,6 @@ document.addEventListener("DOMContentLoaded", function() {
     //     kernelOutput.textContent = "Mascara Utilizada: "+kernelInput.value;
 
     // });
-    document.getElementById("original").textContent = " ";
+    // document.getElementById("original").textContent = " ";
 });
 
